cc7c4b63f755f728cb7ec0ce2422c7d835fdde6f
ebof1223 <erwinjbofetiado@gmail.com>
2024-12-01 22:39:51 -0600

    last ass

--- init.lua
+++ init.lua
@@ -607,7 +607,7 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
   sources = {
-    -- null_ls.builtins.diagnostics.flake8,
+    null_ls.builtins.diagnostics.flake8,
     null_ls.builtins.diagnostics.mypy,
     -- null_ls.builtins.diagnostics.ruff,
     null_ls.builtins.formatting.prettier,
